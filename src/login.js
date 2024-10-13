c9c0bd39243111f2be0ac2e7556f6b17af6a687d
miinimanimo <ssk07079@gmail.com>
2024-10-13 21:31:12 +0900

    fix: 로그인 관련 함수 변경

--- src/login.js
+++ src/login.js
@@ -1,36 +1,26 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import GoogleLoginButton from './GoogleLoginButton';
-import './styles.css';
+import React from 'react';
+import './styles.css';  // 스타일을 적용하려면 필요
+import { apiFetch } from './api';
 
-const Login = ({isLoggedIn, setIsLoggedIn}) => {
-    // const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [userName, setUserName] = useState('');
-    const navigate = useNavigate();
-
-    const handleLoginSuccess = (decoded, token) => {
-        setUserName(decoded.name);  // 구글에서 받은 사용자 이름 설정
-        localStorage.setItem('accessToken', token);  // 토큰을 로컬 스토리지에 저장
-        setIsLoggedIn(true);
-        navigate('/');  // 로그인 성공 시 홈으로 리다이렉트
+const Login = () => {
+    // 구글 로그인 버튼 클릭 시 호출되는 함수
+    const handleGoogleLogin = async () => {
+        await apiFetch('/oauth2/authorization/google', {method: 'POST'}) // 백엔드의 구글 OAuth URL로 리디렉션
     };
+
 
     return (
         <div className="login-container">
             <div className="login-box">
                 <img src={`${process.env.PUBLIC_URL}/images/jobbuggarologo.svg`} alt="Jobbug Logo" className="logo" />
+                
+                <button className="google-login-btn" onClick={handleGoogleLogin}>
+                    구글로 로그인
+                </button>
 
-                {isLoggedIn ? (
-                    <p>{userName}님 환영합니다!</p>
-                ) : (
-                    <GoogleLoginButton onLoginSuccess={handleLoginSuccess} setIsLoggedIn={setIsLoggedIn} />
-                )}
-
-                {!isLoggedIn && (
-                    <div className="signup-section">
-                        <p>아직 회원이 아닌가요? <a href="/register" className="signup-link">회원가입 하러가기</a></p>
-                    </div>
-                )}
+                <div className="signup-section">
+                    <p>아직 회원이 아닌가요? <a href="/register" className="signup-link">회원가입 하러가기</a></p>
+                </div>
             </div>
         </div>
     );
